a16e42ffe27adb12fdcdb6da7f8a67b4e28322a3
Timothy jan <jan.timothy@gmail.com>
2024-10-29 11:11:57 -0700

    Remove launchSettings.json from tracking

--- PplPro/PplPro.Server/obj/Debug/net8.0/PplPro.Server.AssemblyInfo.cs
+++ PplPro/PplPro.Server/obj/Debug/net8.0/PplPro.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PplPro.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665b5d6219fd00d15f98ef66da64e385941d287c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4f06dbbc9e94ff792835a78a49ba17e42cd04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PplPro.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PplPro.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
