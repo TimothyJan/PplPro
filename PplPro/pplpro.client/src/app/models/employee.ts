6598222cba2f324410860b5485a5a088280b2466
Timothy jan <jan.timothy@gmail.com>
2024-11-05 14:52:15 -0800

    Models Created

--- PplPro/pplpro.client/src/app/models/employee.ts
+++ PplPro/pplpro.client/src/app/models/employee.ts
@@ -1,17 +1,17 @@
-import { Department } from "./department";
-import { Role } from "./role";
-
 export class Employee {
   employeeID: number = 0;
   name: string = "";
   position: string = "";
   salary: number = 0;
   departmentID: number = 0;
-  department: Department  = new Department();
   roleID: number = 0;
-  role: Role = new Role();
 
-  constructor(init?: Partial<Employee>) {
-    Object.assign(this, init);
+  constructor(employeeID: number, name: string, position: string, salary: number, departmentID: number, roleID: number) {
+    this.employeeID = employeeID;
+    this.name = name;
+    this.position = position;
+    this.salary = salary;
+    this.departmentID = departmentID;
+    this.roleID = roleID;
   }
 }
--- PplPro/pplpro.client/src/app/models/role.ts
+++ PplPro/pplpro.client/src/app/models/role.ts
@@ -1,8 +1,11 @@
 export class Role {
   roleID: number = 0;
   roleName: string = "";
+  departmentID: number = 0;
 
-  constructor(init?: Partial<Role>) {
-    Object.assign(this, init);
+  constructor(roleID: number, roleName: string, departmentID: number = 0,) {
+    this.roleID = roleID;
+    this.roleName = roleName;
+    this.departmentID = departmentID;
   }
 }
